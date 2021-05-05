43e93e3dfe156992a00e5d98538c0b09bc2137d6
rodrigomnnds <51718701+rodrigomnnds@users.noreply.github.com>
2021-05-05 00:05:54 -0300

    Add files via upload

--- Estruturas_de_Dados.c
+++ Estruturas_de_Dados.c
@@ -4,33 +4,64 @@
 //--------------------------------------------
 //pilha
 //--------------------------------------------
-typedef struct sNopilha{
+typedef struct sNo_pilha{
 	Carta carta;
-	int tam_atual;
-	int tam_max;
-	struct sNoPilha *prox;
-}Nopilha;
+	int n_cartas;
+	int n_max_cartas;
 
-Nopilha *inicializar(){
-	Nopilha *novoNoPilha = (Nopilha*)malloc(sizeof(Nopilha));
-	novoNoPilha->prox = NULL;
-	novoNoPilha->tam_atual = 0;
-	return novoNoPilha;
+}No_pilha;
+
+void pilha_alocar_no(No_pilha *ptr_externo_pilha){
+	ptr_externo_pilha = (No_pilha*)malloc(sizeof(No_pilha));
 }
 
-int isVazia(Nopilha *ptr_externo_pilha){
-	if(ptr_externo_pilha->prox == NULL){
+void pilha_inicializar(No_pilha **ptr_externo_pilha, int n){
+	pilha_alocar_no(*ptr_externo_pilha);
+	//foo->bar is equivalent to (*foo).bar, i.e. it gets the member called bar from the struct that foo points to.
+	//não sei pq não funciona com:
+	//*ptr_externo_pilha->n_max_cartas = n;
+	(*ptr_externo_pilha)->n_max_cartas = n;
+	(*ptr_externo_pilha)->n_cartas = 0;
+
+	
+}
+int pilha_is_vazia(No_pilha *ptr_externo_pilha){
+	if(ptr_externo_pilha->n_cartas == 0){
 		return 1;
 	}
 	else{
 		return 0;
 	}
 }
-
-Nopilha *push(Nopilha *ptr_externo_pilha, Carta carta){
-	Nopilha *novoNoPilha;
+int pilha_is_cheia(No_pilha *ptr_externo_pilha){
+	if(ptr_externo_pilha->n_cartas == ptr_externo_pilha->n_max_cartas){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
+void pilha_push(){}
+void posh_push(){}
+void peek(){}
+//--------------------------------------------
+//fila
+//--------------------------------------------
+//--------------------------------------------
+//deque
+//--------------------------------------------
 
+
+int main(){
+	No_pilha *ptr_externo_pilha;
+	pilha_inicializar(&ptr_externo_pilha, 0);
+	if(pilha_is_cheia(ptr_externo_pilha)){
+		printf("A fila foi alocada com sucesso e está cheia\n");
+	}
+
+
+
+};
 
 
 
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "./funcoes.c"
+/*
+
 int main(){
 
 
@@ -13,30 +15,6 @@ int main(){
 }
 
 
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include "Estruturas_de_Dados.c"
-//#include "Estrutura_Carta.h"
 
-typedef struct sCarta{
-	int valor;
-	int nipe;
-}Carta;
-
-
-int main(){
-	Nopilha *pilha;
-	pilha = inicializar();
-	if (isVazia(pilha))
-		printf("pilha vazia\n");
-		pilha = inicializar();
-	Carta nova_carta;
-	nova_carta.valor = 10;
-	nova_carta.nipe = 1;
-	if (isVazia(pilha))
-		printf("pilha vazia\n");
-	return 0;
-}
 
 */
